a1d06eaf86654dff00d70d04677b7f7c03d2b9a8
Enosh Tsur <enosh.tsur@unframe.ai>
2025-05-19 10:01:21 +0300

    assets

--- assets.js
+++ assets.js
@@ -1,5 +1,4 @@
-
-// Add with other image declarations at the top of the file
+// ================= NPC Sprites =================
 const npcImg = new Image();
 npcImg.src = 'niro_standing.png';
 
@@ -12,37 +11,25 @@ npcInjuredImg.src = 'niro_injured.png';
 const npcFallingImg = new Image();
 npcFallingImg.src = 'niro_falling.png';
 
+// ================= Lamp =================
 const lampImg = new Image();
 lampImg.src = 'lamp.png';
-// Load wall texture
+const lampCount = 20; // Number of lamps to spawn
+const lamps = [];
+
+// ================= Wall Textures =================
 const wallTexture = new Image();
 wallTexture.src = 'satanic-wall1.png';
 
-// Load building wall texture
 const buildingWallTexture = new Image();
 buildingWallTexture.src = 'satanic_wall2.png';
 
-// Load new wall textures
 const wall1Texture = new Image();
 wall1Texture.src = 'satanic_walld3.png';
 
 const door2Texture = new Image();
 door2Texture.src = 'door_2.png';
 
-// Load DOOM Guy face images for animation
-const doomGuyFaceNormal = new Image();
-doomGuyFaceNormal.src = './doom_guy1.png';
-
-const doomGuyFaceBlink = new Image();
-doomGuyFaceBlink.src = './doom_guy_blinks.png';
-
-const doomGuyFaceSmile = new Image();
-doomGuyFaceSmile.src = './doom_guy_smile.png';
-
-const lampCount = 20; // Number of lamps to spawn
-const lamps = [];
-
-// Load additional wall textures
 const doomWall2Texture = new Image();
 doomWall2Texture.src = 'satanic_walld2.png';
 
@@ -55,6 +42,17 @@ doomDoorTexture.src = 'doom_door.png';
 const magicWallTexture = new Image();
 magicWallTexture.src = 'static_wall3.png';
 
+// ================= UI/DOOM Guy Face =================
+const doomGuyFaceNormal = new Image();
+doomGuyFaceNormal.src = './doom_guy1.png';
+
+const doomGuyFaceBlink = new Image();
+doomGuyFaceBlink.src = './doom_guy_blinks.png';
+
+const doomGuyFaceSmile = new Image();
+doomGuyFaceSmile.src = './doom_guy_smile.png';
+
+// ================= Weapon Sprites =================
 const handgunSrc = 'handgun.png';
 const handgunImages = [];
 const handgunImg = new Image();
@@ -65,8 +63,14 @@ const shotgunImages = [];
 const shotgunImg = new Image();
 shotgunImg.src = shotgunSrc;
 
+const lizergunSrc = 'liser_fps.png';
+const lizergunImages = [];
+const lizergunImg = new Image();
+lizergunImg.src = lizergunSrc;
+
+// ================= Pickup Sprites =================
 const shotgunPickups = [];
-const maxShotgunPickups = 15; // Changed to 7 pickups
+const maxShotgunPickups = 15;
 const shotgunPickupImg = new Image();
 shotgunPickupImg.src = 'drop_shootgun.png';
 
@@ -75,40 +79,30 @@ const maxLizergunPickups = 15;
 const lizergunPickupImg = new Image();
 lizergunPickupImg.src = 'lizergun.png';
 
-const lizergunSrc = 'liser_fps.png';
-const lizergunImages = [];
-const lizergunImg = new Image();
-lizergunImg.src = lizergunSrc;
-
-// --- Load NPC hurt sound ---
+// ================= Sounds =================
 const npcHurtSound = new Audio('niro_hurt.wav');
-npcHurtSound.volume = 1.0; // Set as desired
+npcHurtSound.volume = 1.0;
 
-// --- Load shotgun fire sound ---
 const shotgunSound = new Audio('shotgun.wav');
-shotgunSound.volume = 1.0; // Set as desired
+shotgunSound.volume = 1.0;
 
-// --- Load liser fire sound ---
 const liserSound = new Audio('liser.wav');
-liserSound.volume = 1.0; // Set as desired
+liserSound.volume = 1.0;
 
-// --- Load pistol fire sound ---
 const pistolSound = new Audio('pistol.wav');
-pistolSound.volume = 1.0; // Set as desired
+pistolSound.volume = 1.0;
 
-// --- Load door open sound ---
 const doorOpenSound = new Audio('dooropen.wav');
-doorOpenSound.volume = 1.0; // Set as desired
+doorOpenSound.volume = 1.0;
 
-// --- Load door close sound ---
 const doorCloseSound = new Audio('doorclose.wav');
-doorCloseSound.volume = 1.0; // Set as desired
+doorCloseSound.volume = 1.0;
 
-// --- Load weapon pickup sound ---
 const weaponPickupSound = new Audio('wpick.wav');
-weaponPickupSound.volume = 1.0; // Set as desired
+weaponPickupSound.volume = 1.0;
 
+// ================= Music =================
 const bgMusic = new Audio('doom_gate.mp3');
 bgMusic.loop = true;
-bgMusic.volume = 1.0; // Set volume as desired
+bgMusic.volume = 1.0;
 let musicStarted = false;
